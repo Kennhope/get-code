c128fefef9b6135b2b385bbfdebf4faee50f0697
Kennhope <62108078+Kennhope@users.noreply.github.com>
2020-03-12 11:56:09 -0400

    Update format method.py

--- format method.py
+++ format method.py
@@ -2,4 +2,4 @@ age = 19
 name = 'Ky'
 
 print ('{0} was {1} years old when he decided to learn Python'.format (name, age))
-print ('Why {0} decided to learn Python?'.format (name, age))
+print ('Why {0} decided to learn Python?'.format (name))
